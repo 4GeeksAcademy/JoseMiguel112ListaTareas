5e3522625d3f7f385a51b094fae2d7c01269ce1f
Jose <jm.bebor@gmail.com>
2025-06-16 11:12:17 +0000

    [broken]

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [tarea, setTarea] = useState("")
+	const [listaTareas, setListaTareas] = useState([])
+	console.log(listaTareas)
+	function updateList(event) {
+		event.preventDefault();
+
+
+		if (event.key === "Enter") {
+			setListaTareas(listaTareas.concat(tarea));
+		}
+	}
+
+	
+	const arrayDetareas = listaTareas.map((item)=><li>{item}</li>)
+	
+
 	return (
 		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+			<h1 className="text-center mt-5 text-secondary">Todos</h1>
+			
+
+				<input type="text" className="form-control" value={tarea} onChange={(event) => setTarea(event.target.value)} onKeyDown={updateList} />
+		
+			<ul>
+				{arrayDetareas}
+			</ul>
 		</div>
 	);
 };
 
 export default Home;
+
